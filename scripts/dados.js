6c82a7046ff928e25eb6b5974530c209476eea13
Caio Filardis <caiosantosshake@hotmail.com>
2026-01-12 21:08:11 -0300

    Deploy

---
+++ scripts/dados.js
@@ -0,0 +1,115 @@
+const MENU = {
+    "bolos": [
+        {
+            "id": "bolo-chocolate-ganache",
+            "img": "https://images.unsplash.com/photo-1571115177098-24ec42ed204d?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+            "name": "Bolo de Chocolate",
+            "dsc": "Delicioso bolo de chocolate com recheio de ganache e decoração de frutas vermelhas.",
+            "price": 45.00
+        },
+        {
+            "id": "bolo-red-velvet",
+            "img": "https://images.unsplash.com/photo-1586788680434-30d32443d468?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+            "name": "Red Velvet",
+            "dsc": "Massa vermelha aveludada com recheio clássico de cream cheese e baunilha.",
+            "price": 50.00
+        },
+        {
+            "id": "bolo-cenoura-chocolate",
+            "img": "https://images.unsplash.com/photo-1621303837174-89787a7d4729?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+            "name": "Bolo de Cenoura",
+            "dsc": "O clássico caseiro: massa fofinha de cenoura com cobertura vulcão de brigadeiro.",
+            "price": 35.00
+        },
+        {
+            "id": "bolo-limao-siciliano",
+            "img": "https://images.unsplash.com/photo-1519340333755-56e9c1d04579?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+            "name": "Bolo de Limão",
+            "dsc": "Bolo refrescante de limão siciliano com cobertura glacê de açúcar.",
+            "price": 38.00
+        }
+    ],
+    "cookies-e-doces": [
+        {
+            "id": "cookie-classico",
+            "img": "https://images.unsplash.com/photo-1552689486-f6773047d19f?ixlib=rb-1.2.1&auto=format&fit=crop&w=651&q=80",
+            "name": "Cookies Clássicos",
+            "dsc": "Biscoitos crocantes por fora e macios por dentro com gotas de chocolate belga.",
+            "price": 8.00
+        },
+        {
+            "id": "macarons-sortidos",
+            "img": "https://images.unsplash.com/photo-1569864358642-9d1684040f43?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+            "name": "Macarons (Unid.)",
+            "dsc": "Delicados macarons franceses. Sabores: Pistache, Framboesa e Baunilha.",
+            "price": 6.50
+        },
+        {
+            "id": "brownie-nozes",
+            "img": "https://images.unsplash.com/photo-1606313564200-e75d5e30476d?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+            "name": "Brownie com Nozes",
+            "dsc": "Brownie denso e úmido com pedaços crocantes de nozes.",
+            "price": 12.00
+        },
+        {
+            "id": "cupcake-baunilha",
+            "img": "https://images.unsplash.com/photo-1576618148400-f54bed99fcf8?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+            "name": "Cupcake Floral",
+            "dsc": "Massa de baunilha com cobertura de buttercream decorado com flores.",
+            "price": 10.00
+        }
+    ],
+    "tortas": [
+        {
+            "id": "torta-frutas",
+            "img": "https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+            "name": "Torta de Frutas",
+            "dsc": "Massa leve (Pâte Sablée) recheada com creme de confeiteiro e frutas frescas.",
+            "price": 55.00
+        },
+        {
+            "id": "cheesecake-frutas-vermelhas",
+            "img": "https://images.unsplash.com/photo-1533134242443-d4fd215305ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+            "name": "Cheesecake NY",
+            "dsc": "Tradicional cheesecake estilo Nova York com calda de frutas vermelhas.",
+            "price": 60.00
+        },
+        {
+            "id": "torta-limao-merengue",
+            "img": "https://images.unsplash.com/photo-1519915028121-7d3463d20b13?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+            "name": "Torta de Limão",
+            "dsc": "Base crocante, creme azedinho de limão e cobertura de merengue maçaricado.",
+            "price": 48.00
+        },
+        {
+            "id": "banoffee-pie",
+            "img": "https://images.unsplash.com/photo-1616031037011-087000171ea3?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+            "name": "Banoffee",
+            "dsc": "Camadas de banana, doce de leite artesanal e chantilly fresco.",
+            "price": 52.00
+        }
+    ],
+    "bebidas": [
+        {
+            "id": "cafe-espresso",
+            "img": "https://images.unsplash.com/photo-1514432324607-a09d9b4aefdd?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+            "name": "Espresso Duplo",
+            "dsc": "Café de grãos selecionados da Colômbia, torra média.",
+            "price": 6.00
+        },
+        {
+            "id": "cappuccino-cremoso",
+            "img": "https://images.unsplash.com/photo-1572442388796-11668a67e53d?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+            "name": "Cappuccino",
+            "dsc": "Espresso, leite vaporizado e chocolate em pó.",
+            "price": 10.00
+        },
+        {
+            "id": "cha-artesanal",
+            "img": "https://images.unsplash.com/photo-1556679343-c7306c1976bc?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+            "name": "Chá de Hibisco",
+            "dsc": "Infusão gelada de hibisco com limão e hortelã.",
+            "price": 8.00
+        }
+    ]
+};
